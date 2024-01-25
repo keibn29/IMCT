4551f17b970e64fc9243ae0536fc3825d777b4eb
keibn29 <keibn29@gmail.com>
2024-01-26 03:56:39 +0700

    [UPDATE] navigate menu

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,44 +1,32 @@
 import { DownOutlined, SearchOutlined } from '@ant-design/icons';
 import { Flex, Menu, Select, Space } from 'antd';
+import useHeaderStyle from 'app/hooks/useHeaderStyle';
 import { languageOptions, menuItems } from 'constant/nav';
+import { URL } from 'constant/url';
 import Logo from 'images/logo.svg?react';
-import { useEffect, useState } from 'react';
-
-type HeaderBackgroundColor = 'transparent' | '#17363B';
-const SCROLL_POSITION_CHANGE_BACKGROUND = 700;
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const [headerBackgroundColor, setHeaderBackgroundColor] = useState<HeaderBackgroundColor>('transparent');
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const nextHeaderBackground = window.scrollY >= SCROLL_POSITION_CHANGE_BACKGROUND ? '#17363B' : 'transparent';
-      setHeaderBackgroundColor(nextHeaderBackground);
-    };
-
-    window.addEventListener('scroll', handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const navigate = useNavigate();
+  const { headerBackgroundColor, headerClassName } = useHeaderStyle();
 
   return (
     <Flex
-      className={`fixed h-[100px] text-white w-[100vw] z-[99]`}
+      className={`${headerClassName} h-[100px] text-white w-full z-[99]`}
       style={{ backgroundColor: headerBackgroundColor }}
       justify="center"
       align="center"
     >
       <Flex className="container" justify="space-between">
-        <Space className="">
-          <Logo />
+        <Space className="cursor-pointer">
+          <Logo onClick={() => navigate(URL.Home)} />
         </Space>
         <Menu
           className="justify-center items-center text-white gap-[30px] text-lg font-light header-menu select-none"
           mode="horizontal"
           defaultSelectedKeys={[]}
           items={menuItems}
+          onClick={(evt: any) => navigate(evt.item.props.link)}
         />
         <Flex align="center" gap={10}>
           <SearchOutlined className="text-xl" />
